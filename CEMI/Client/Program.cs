b96b7196ba17791f2f141d1dca4b3cdf76486362
Nabil <nabil.ziani@brightest.be>
2023-06-17 21:16:07 +0200

    using mudblazor to render students overview with proper design and search and pagination functionality

--- CEMI/Client/Program.cs
+++ CEMI/Client/Program.cs
@@ -4,12 +4,14 @@ global using CEMI.Client.Shared;
 using CEMI.Client;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using MudBlazor.Services;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
-builder.Services.AddScoped<IStudentService, StudentService>();  
+builder.Services.AddMudServices();
+builder.Services.AddScoped<IStudentService, StudentService>();
 
 await builder.Build().RunAsync();
